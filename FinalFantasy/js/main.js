fc4174a39cae2114f14c87c0dcabdfe13faa4de6
Michaelpkr <michael.parker@ramseysolutions.com>
2022-04-04 17:54:52 -0500

    commit

---
+++ FinalFantasy/js/main.js
@@ -0,0 +1,31 @@
+document.getElementById('FF1').onclick = ff1Background
+document.getElementById('FF2').onclick = ff2Background
+document.getElementById('FF3').onclick = ff3Background
+document.getElementById('FF4').onclick = ff4Background
+document.getElementById('FF5').onclick = ff5Background
+
+
+function ff1Background() {
+    document.body.style.backgroundImage = "url('/Users/michael.parker/Desktop/class12-materials/background-picker/img/ff1BackgroundIMG.png')";
+    document.querySelector('body').style.color = 'white';
+}
+
+function ff2Background() {
+    document.querySelector('body').style.backgroundImage = ''
+    document.querySelector('body').style.color = 'white'
+}
+
+function ff3Background() {
+    document.querySelector('body').style.backgroundImage = ''
+    document.querySelector('body').style.color = 'white'
+}
+
+function ff4Background() {
+    document.querySelector('body').style.backgroundImage = ''
+    document.querySelector('body').style.color = 'white'
+}
+
+function ff5Background() {
+    document.querySelector('body').style.backgroundImage = ''
+    document.querySelector('body').style.color = 'white'
+}
