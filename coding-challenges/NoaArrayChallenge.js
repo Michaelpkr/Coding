940b4cdae12ad194ce6518294f8d78a172141183
Michaelpkr <michael.parker@ramseysolutions.com>
2022-05-24 19:21:44 -0500

    I'm adding in this message and then trying to get out of vim

---
+++ coding-challenges/NoaArrayChallenge.js
@@ -0,0 +1,26 @@
+const users = [
+    "Michael",
+    "Heather",
+    "Ben",
+    "Savannah",
+    "Miguel",
+    "Noa",
+    "Emily",
+    "Bing",
+    "Barnes"
+];
+ 
+// Given an array of users, write a function that goes through the array and checks to see if a specific user exists in the array
+// inputs:
+// String => name of user
+// Array => array of users to be checked
+ 
+// output:
+//Boolean => true or false
+ 
+function isUserPresent(firstName, listOfUsers) {
+    // write code below
+ 
+}
+
+//For this to work, I would need the function to first accept a value from the user who is running the function, that would be called "userName". "userName" would be a parameter in the function itself. Within the function, I would have a forEach Loop that would look at the array, and each element within it. if "userName" and an element within the array match, the function would log true, with an else statement of logging false.
