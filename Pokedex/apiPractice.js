d3ab117cafab1693b028f9b781a6c516975cb982
Michaelpkr <michael.parker@ramseysolutions.com>
2022-05-26 20:56:09 -0500

    Adding pokex

---
+++ Pokedex/apiPractice.js
@@ -0,0 +1,19 @@
+document.querySelector("button").addEventListener("click", getPokemon)
+
+
+function getPokemon(){
+
+    let pokemon = document.querySelector("input").value
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+   document.querySelector("h1").innerText = (data.species.name)
+  document.querySelector("img").src = (data.sprites.front_default)
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
+
+}
