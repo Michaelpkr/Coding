af436bd304b5eba1c162b739f8f064c44e895c67
Michaelpkr <92385361+Michaelpkr@users.noreply.github.com>
2022-06-06 14:40:32 -0500

    Sync

--- Pokedex/apiPractice.js
+++ Pokedex/apiPractice.js
@@ -1,19 +1,20 @@
 document.querySelector("button").addEventListener("click", getPokemon)
 
 
-function getPokemon(){
+function getPokemon() {
 
     let pokemon = document.querySelector("input").value
 
-fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
-.then(res => res.json())
-.then(data => {
-    console.log(data)
-   document.querySelector("h1").innerText = (data.species.name)
-  document.querySelector("img").src = (data.sprites.front_default)
-})
-.catch(err => {
-    console.log(`error ${err}`)
-});
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector("h1").innerText = (data.species.name)
+            document.querySelector("img").src = (data.sprites.front_default)
+            document.querySelector("h3").innerText = (data.height)
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
 
 }
